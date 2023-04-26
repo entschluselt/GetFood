5f2409ea091e62a0e37b80d20ff18225643ba4e1
entschluselt <yashvendradeep@gmail.com>
2023-04-26 11:32:14 +0530

    second stage

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,21 +1,15 @@
 import React from 'react'
 import NavBar from '../Components/navbar'
 import Footer from '../Components/Footer'
+import Card from '../Components/card'
+import Carousel from '../Components/Carousel'
 export default function
   () {
   return (
     <div>
       <div> <NavBar /> </div>
-      <div>
-        <div ClassName="card mt-3" style={{"width": "18rem"}}>
-          <img src="..." ClassName="card-img-top" alt="..."/>
-            <div ClassName="card-body">
-              <h5 ClassName="card-title">Card title</h5>
-              <p ClassName="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              
-            </div>
-        </div>
-      </div>
+      <div> <Carousel/> </div>
+      <div> <Card/> </div>
       <div> <Footer /> </div>
     </div>
   )
